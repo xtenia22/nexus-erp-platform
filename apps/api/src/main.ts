5f477d37e135834cee7ff43bf31b0b7cc23d145c
Leonardo Diaz <diazleonardomatias@gmail.com>
2026-05-02 13:29:08 -0300

    feat: allow orgins Dev in Next.config.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,7 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
  
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin:'http://localhost:3000',
+    
   });
 
   app.setGlobalPrefix('api');
